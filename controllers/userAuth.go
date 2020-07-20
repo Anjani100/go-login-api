c69343629b251bd432142c173360d526804aad23
Anjani100 <anjani10.kumar@gmail.com>
2020-07-20 14:45:43 +0530

    UserName changed to Username.

---
+++ controllers/userAuth.go
@@ -0,0 +1,22 @@
+package controllers
+
+import (
+	"net/http"
+	"github.com/gin-gonic/gin"
+	"github.com/Anjani100/go-login-api/models"
+)
+
+type CreateUserInput struct {
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}	// UserID will be generated automatically by the database
+
+func CreateUser(c *gin.Context) {
+	var input CreateUserInput
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	user := models.User{Username: input.Username, Password: input.Password}
+}
